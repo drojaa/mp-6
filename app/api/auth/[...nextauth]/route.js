739595a3e0cea7d57df59bcbd72c43eced0c20e2
drojaa <droja@bu.edu>
2024-12-06 22:04:56 -0500

    Updated env fail

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,8 +4,8 @@ import GoogleProvider from 'next-auth/providers/google';
 const authOptions = {
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            clientId: process.env.local.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.local.GOOGLE_CLIENT_SECRET,
         }),
     ],
 };
